363abf58eb0f24cc330d4832319a0f35135e24ae
Bhanu <uabhanu@gmail.com>
2023-06-15 15:08:49 +0100

    PlayerController updated to accomodate Turn Based 3 players system that places coins only on valid cells only if they are empty and on Mouse Click.

--- BobProximity/Assets/Scripts/PlayerController.cs
+++ BobProximity/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,22 +6,57 @@ public class PlayerController : MonoBehaviour
 {
     private GridManager _gridManager;
     private InputActions _playerInputActions;
+    private int _currentPlayer = 0;
+    private Vector2Int _cellIndexAtMousePosition;
+
+    [SerializeField] private GameObject coinObj;
+    [SerializeField] private TMP_Text coinValueLabelTMP;
 
     private void Start()
     {
         _gridManager = FindObjectOfType<GridManager>();
         _playerInputActions = new InputActions();
         _playerInputActions.ProximityMap.Enable();
+        
+        StartPlayerTurn();
     }
 
     private void Update()
     {
         Vector3 mouseScreenPos = Mouse.current.position.ReadValue();
         mouseScreenPos.z = Camera.main.nearClipPlane;
-        
+
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-        Vector2Int cellIndexAtMouseWorldPos = _gridManager.WorldToCell(mouseWorldPos);
-        
-        Debug.Log("Cell Index : " + cellIndexAtMouseWorldPos);
+        _cellIndexAtMousePosition = _gridManager.WorldToCell(mouseWorldPos);
+
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if(_cellIndexAtMousePosition == _gridManager.InvalidCellIndex)
+            {
+                //Debug.Log("Invalid Cell : " + _gridManager.InvalidCellIndex + " as Current Cell Index : " + _cellIndexAtMousePosition);
+                return;
+            }
+            
+            if(!_gridManager.IsCellBlocked.GetValue(_cellIndexAtMousePosition.x , _cellIndexAtMousePosition.y))
+            {
+                //Debug.Log("Not Invalid Cell : " + _gridManager.InvalidCellIndex + " as Current Cell Index : " + _cellIndexAtMousePosition);
+                Vector2 spawnPos = _gridManager.CellToWorld(_cellIndexAtMousePosition.x , _cellIndexAtMousePosition.y);
+                Instantiate(coinObj , spawnPos , Quaternion.identity , gameObject.transform);
+                _gridManager.IsCellBlocked.SetValue(_cellIndexAtMousePosition.x , _cellIndexAtMousePosition.y , true);
+                EndPlayerTurn();
+                StartPlayerTurn();
+            }
+        }
+    }
+
+    private void StartPlayerTurn()
+    {
+        Debug.Log("Player " + (_currentPlayer + 1) + "'s Turn");   
+    }
+
+    private void EndPlayerTurn()
+    {
+        Debug.Log("Player " + (_currentPlayer + 1) + "'s Turn Ended");
+        _currentPlayer = (_currentPlayer + 1) % 3;
     }
 }
