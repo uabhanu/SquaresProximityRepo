0c35299cd40c92816f616cdd279ec5b35a9704be
Bhanu <uabhanu@gmail.com>
2023-06-17 17:23:17 +0100

    Changed the logic of scoring and currently, the adding to the list and removal from the list based on coin placed and captured is working correctly.

--- BobProximity/Assets/Scripts/ScoreManager.cs
+++ BobProximity/Assets/Scripts/ScoreManager.cs
@@ -1,15 +1,29 @@
 using Event = Events.Event;
 using Events;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] private int[] scoreValuesArray;
-    [SerializeField] private TMP_Text[] scoreValueLabelsArray;
+    private int _blueScoreValue;
+    private int _greenScoreValue;
+    private int _redScoreValue;
+    
+    private List<int> _blueCoinValuesList;
+    private List<int> _greenCoinValuesList;
+    private List<int> _redCoinValuesList;
+
+    [SerializeField] private TMP_Text blueCoinValueTMP;
+    [SerializeField] private TMP_Text greenCoinValueTMP;
+    [SerializeField] private TMP_Text redCoinValueTMP;
 
     private void Start()
     {
+        _blueCoinValuesList = new List<int>();
+        _greenCoinValuesList = new List<int>();
+        _redCoinValuesList = new List<int>();
+        
         SubscribeToEvents();
     }
 
@@ -18,20 +32,51 @@ public class ScoreManager : MonoBehaviour
         UnsubscribeFromEvents();
     }
 
+    private void OnCoinCaptured(int coinValue , int playerID)
+    {
+        if(playerID == 0)
+        {
+            _redCoinValuesList.Remove(coinValue);
+        }
+
+        if(playerID == 1)
+        {
+            _greenCoinValuesList.Remove(coinValue);
+        }
+        
+        if(playerID == 2)
+        {
+            _blueCoinValuesList.Remove(coinValue);
+        }
+    }
+
     private void OnCoinPlaced(int coinValue , int playerID)
     {
-        //Debug.Log("Player " + playerID + " placed the coin and " + " coin value : " + coinValue);
-        scoreValuesArray[playerID] += coinValue;
-        scoreValueLabelsArray[playerID].text = scoreValuesArray[playerID].ToString();
+        if(playerID == 0)
+        {
+            _redCoinValuesList.Add(coinValue);
+        }
+
+        if(playerID == 1)
+        {
+            _greenCoinValuesList.Add(coinValue);
+        }
+        
+        if(playerID == 2)
+        {
+            _blueCoinValuesList.Add(coinValue);
+        }
     }
 
     private void SubscribeToEvents()
     {
+        EventsManager.SubscribeToEvent(Event.CoinCaptured , OnCoinCaptured);
         EventsManager.SubscribeToEvent(Event.CoinPlaced , OnCoinPlaced);
     }
 
     private void UnsubscribeFromEvents()
     {
+        EventsManager.UnsubscribeFromEvent(Event.CoinCaptured , OnCoinCaptured);
         EventsManager.UnsubscribeFromEvent(Event.CoinPlaced , OnCoinPlaced);
     }
 }
