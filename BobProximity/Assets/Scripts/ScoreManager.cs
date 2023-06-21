d4fc56e2cf37fca5d7b3c34c1f3911ca3e7727be
Bhanu <uabhanu@gmail.com>
2023-06-21 16:25:01 +0100

    Cleaned up

--- BobProximity/Assets/Scripts/ScoreManager.cs
+++ BobProximity/Assets/Scripts/ScoreManager.cs
@@ -3,115 +3,42 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    private int _redCoinScoreValue;
-    private int _greenCoinScoreValue;
-    private int _blueCoinScoreValue;
+    private int[] _coinScoreValues;
 
     [SerializeField] private GameObject gameOverPanelsObj;
-    [SerializeField] private TMP_Text blueCoinScoreTMP;
-    [SerializeField] private TMP_Text greenCoinScoreTMP;
-    [SerializeField] private TMP_Text redCoinScoreTMP;
+    [SerializeField] private TMP_Text[] coinScoreTexts;
 
     private void Start()
     {
         gameOverPanelsObj.SetActive(false);
+        _coinScoreValues = new int[3];
         UpdateScoreTexts();
     }
     
-    public void CoinBuffedUpScore(int buffedUpCoinPlayerID, int buffedUpCoinIncrement)
+    public void CoinBuffedUpScore(int buffedUpCoinPlayerID , int buffedUpCoinIncrement)
     {
-        if(buffedUpCoinPlayerID == 0)
-        {
-            _redCoinScoreValue += buffedUpCoinIncrement;
-        }
-        
-        else if(buffedUpCoinPlayerID == 1)
-        {
-            _greenCoinScoreValue += buffedUpCoinIncrement;
-        }
-        
-        else if(buffedUpCoinPlayerID == 2)
-        {
-            _blueCoinScoreValue += buffedUpCoinIncrement;
-        }
-
+        _coinScoreValues[buffedUpCoinPlayerID] += buffedUpCoinIncrement;
         UpdateScoreTexts();
     }
 
     public void CoinCapturedScore(int capturingPlayerID , int capturedPlayerID , int capturedCoinValue)
     {
-        if(capturingPlayerID == 0)
-        {
-            _redCoinScoreValue += capturedCoinValue;
-            
-            if(capturedPlayerID == 1)
-            {
-                _greenCoinScoreValue -= capturedCoinValue;
-            }
-            
-            else if(capturedPlayerID == 2)
-            {
-                _blueCoinScoreValue -= capturedCoinValue;
-            }
-        }
-        
-        else if(capturingPlayerID == 1)
-        {
-            _greenCoinScoreValue += capturedCoinValue;
-            
-            if(capturedPlayerID == 0)
-            {
-                _redCoinScoreValue -= capturedCoinValue;
-            }
-            
-            else if(capturedPlayerID == 2)
-            {
-                _blueCoinScoreValue -= capturedCoinValue;
-            }
-        }
-        
-        else if(capturingPlayerID == 2)
-        {
-            _blueCoinScoreValue += capturedCoinValue;
-            
-            if(capturedPlayerID == 0)
-            {
-                _redCoinScoreValue -= capturedCoinValue;
-            }
-            
-            else if(capturedPlayerID == 1)
-            {
-                _greenCoinScoreValue -= capturedCoinValue;
-            }
-        }
-
+        _coinScoreValues[capturingPlayerID] += capturedCoinValue;
+        _coinScoreValues[capturedPlayerID] -= capturedCoinValue;
         UpdateScoreTexts();
     }
 
     public void CoinPlacedScore(int coinValue , int playerID)
     {
-        if(playerID == 0)
-        {
-            _redCoinScoreValue += coinValue;
-        }
-        
-        else if(playerID == 1)
-        {
-            _greenCoinScoreValue += coinValue;
-        }
-        
-        else if(playerID == 2)
-        {
-            _blueCoinScoreValue += coinValue;
-        }
-
+        _coinScoreValues[playerID] += coinValue;
         UpdateScoreTexts();
     }
 
     private void UpdateScoreTexts()
     {
-        redCoinScoreTMP.text = _redCoinScoreValue.ToString();
-        greenCoinScoreTMP.text = _greenCoinScoreValue.ToString();
-        blueCoinScoreTMP.text = _blueCoinScoreValue.ToString();
+        for(int i = 0; i < coinScoreTexts.Length; i++)
+        {
+            coinScoreTexts[i].text = _coinScoreValues[i].ToString();
+        }
     }
 }
