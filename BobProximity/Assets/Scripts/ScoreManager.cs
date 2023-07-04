55553bcb555342d4a6143435fd442544988d17be
Bhanu <uabhanu@gmail.com>
2023-07-04 10:42:28 +0100

    Moved the Player Wins Event invocation out of the inner loop so it invokes only once.

--- BobProximity/Assets/Scripts/ScoreManager.cs
+++ BobProximity/Assets/Scripts/ScoreManager.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private int _numberOfPlayers;
-    private int[] _coinScoreValues;
+    private int[] _coinScoreValuesArray;
+    private int[] _playersTotalWinsArray;
     private string[] _playerNamesArray;
 
     [SerializeField] private TMP_Text[] coinScoreTMPTexts;
@@ -24,14 +25,14 @@ public class ScoreManager : MonoBehaviour
 
     private void CoinBuffedUpScore(int buffedUpCoinPlayerID , int buffedUpCoinIncrement)
     {
-        _coinScoreValues[buffedUpCoinPlayerID] += buffedUpCoinIncrement;
+        _coinScoreValuesArray[buffedUpCoinPlayerID] += buffedUpCoinIncrement;
         UpdateScoreTexts();
     }
 
     private void CoinCapturedScore(int capturingPlayerID , int capturedPlayerID , int capturedCoinValue)
     {
-        _coinScoreValues[capturingPlayerID] += capturedCoinValue;
-        _coinScoreValues[capturedPlayerID] -= capturedCoinValue;
+        _coinScoreValuesArray[capturingPlayerID] += capturedCoinValue;
+        _coinScoreValuesArray[capturedPlayerID] -= capturedCoinValue;
         UpdateScoreTexts();
     }
     
@@ -42,9 +43,9 @@ public class ScoreManager : MonoBehaviour
 
         for(int i = 0; i < _numberOfPlayers; i++)
         {
-            if(_coinScoreValues[i] > highestScore)
+            if(_coinScoreValuesArray[i] > highestScore)
             {
-                highestScore = _coinScoreValues[i];
+                highestScore = _coinScoreValuesArray[i];
                 highestScorePlayerID = i;
             }
         }
@@ -56,7 +57,7 @@ public class ScoreManager : MonoBehaviour
     {
         for(int i = 0; i < _numberOfPlayers; i++)
         {
-            coinScoreTMPTexts[i].text = _playerNamesArray[i] + " : " + _coinScoreValues[i];
+            coinScoreTMPTexts[i].text = _playerNamesArray[i] + " : " + _coinScoreValuesArray[i];
         }
     }
 
@@ -72,36 +73,35 @@ public class ScoreManager : MonoBehaviour
 
     private void OnCoinPlaced(int coinValue , int playerID)
     {
-        _coinScoreValues[playerID] += coinValue;
+        _coinScoreValuesArray[playerID] += coinValue;
         UpdateScoreTexts();
     }
 
     private void OnGameOver()
     {
+        int highestScorePlayerID = GetHighestScorePlayer();
+
         for(int i = 0; i < _numberOfPlayers - 1; i++)
         {
             for(int j = i + 1; j < _numberOfPlayers; j++)
             {
-                if(_coinScoreValues[i] == _coinScoreValues[j])
+                if(_coinScoreValuesArray[i] == _coinScoreValuesArray[j])
                 {
-                    //ToDo This logic doesn't account for more than 2 players
+                    EventsManager.Invoke(Event.GameTied);
                     return;
-                }
-                else
-                {
-                    //ToDo This is executing 3 times so fix this but for 2 players, this is working great.
-                    int highestScorePlayerID = GetHighestScorePlayer();
-                    EventsManager.Invoke(Event.PlayerWins , highestScorePlayerID);
                 }
             }
         }
+        
+        EventsManager.Invoke(Event.PlayerWins , highestScorePlayerID , _playersTotalWinsArray);
     }
 
     private void OnNumberOfPlayersSelected(int numberOfPlayers)
     {
         _numberOfPlayers = numberOfPlayers;
-        _coinScoreValues = new int[_numberOfPlayers];
+        _coinScoreValuesArray = new int[_numberOfPlayers];
         _playerNamesArray = new string[_numberOfPlayers];
+        _playersTotalWinsArray = new int[_numberOfPlayers];
     }
     
     private void OnPlayerNamesUpdated(int playerID , string playerName)
