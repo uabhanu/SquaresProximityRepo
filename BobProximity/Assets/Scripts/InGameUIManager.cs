c2314d84b23c37c29b72de6fd86af1daf7d0ed41
Bhanu <uabhanu@gmail.com>
2023-07-07 23:02:11 +0100

    Added a function that gets called when Toggle value is changed and this function invokes RandomTurnsToggled event

--- BobProximity/Assets/Scripts/InGameUIManager.cs
+++ BobProximity/Assets/Scripts/InGameUIManager.cs
@@ -161,6 +161,11 @@ public class InGameUIManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    public void RandomTurnsToggle()
+    {
+        EventsManager.Invoke(Event.RandomTurnsToggled);
+    }
+
     public void ResetButton()
     {
         PlayerPrefsManager.DeleteAll(_playerNamesArray , _playersTotalWinsArray);
