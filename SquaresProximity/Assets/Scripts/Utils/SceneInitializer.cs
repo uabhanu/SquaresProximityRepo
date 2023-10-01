1c53dfd61b81bf79d96db07d405f7cc4d953defa
BHANU-LAPTOP\uabhanu <uabhanu@gmail.com>
2023-10-01 09:57:04 +0100

    Refactored code by adding separate functions for instantiating all the scene objects and the UI prefabs. No change made to the game logic.

--- SquaresProximity/Assets/Scripts/Utils/SceneInitializer.cs
+++ SquaresProximity/Assets/Scripts/Utils/SceneInitializer.cs
@@ -5,27 +5,33 @@ namespace Utils
 {
     public class SceneInitializer : MonoBehaviour
     {
-        //Todo Create an interface IOnSceneLoadInit and use that instead of GameObject
         [SerializeField] private List<GameObject> sceneObjectsList;
         [SerializeField] private List<GameObject> uiObjectsList;
-    
+
         private void Start()
+        {
+            InstantiateSceneObjects();
+            InstantiateUIObjects();
+            Destroy(gameObject);
+        }
+
+        private void InstantiateSceneObjects()
         {
             foreach(var sceneObj in sceneObjectsList)
             {
-                //Todo Use interface to spawn object and call Init() on them
                 Instantiate(sceneObj);
             }
-            
+        }
+
+        private void InstantiateUIObjects()
+        {
             #if UNITY_ANDROID || UNITY_IOS
                 Instantiate(uiObjectsList[0]);
             #endif
-            
+
             #if UNITY_STANDALONE || UNITY_WEBGL
                 Instantiate(uiObjectsList[1]);
             #endif
-        
-            Destroy(gameObject);
         }
     }
 }
