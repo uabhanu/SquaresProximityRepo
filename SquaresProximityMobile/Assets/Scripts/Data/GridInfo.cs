66c8f42230a0db84ee174ce63a23df07012ef25c
BHANU-LAPTOP\uabhanu <uabhanu@gmail.com>
2023-10-01 09:47:21 +0100

    Removed the redundant SquaresProximityMobile Project.

--- SquaresProximityMobile/Assets/Scripts/Data/GridInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class GridInfo
-{
-    public int Rows;
-    public int Cols;
-    public float CellSize;
-}
--- SquaresProximityMobile/Assets/Scripts/Interfaces/ICoinPlacer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Interfaces
-{
-    public interface ICoinPlacer
-    {
-        public void PlaceCoin(Vector2Int cellIndexAtMousePosition);
-    }
-}
