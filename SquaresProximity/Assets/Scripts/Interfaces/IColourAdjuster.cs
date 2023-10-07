e64f3f781a65d65746e43f25d9f31db13e858707
BHANU-LAPTOP\uabhanu <uabhanu@gmail.com>
2023-10-07 23:24:40 +0100

    Added GetContrastingColour function.

--- SquaresProximity/Assets/Scripts/Interfaces/IColourAdjuster.cs
+++ SquaresProximity/Assets/Scripts/Interfaces/IColourAdjuster.cs
@@ -5,6 +5,7 @@ namespace Interfaces
     public interface IColourAdjuster
     {
         public Color GetColor();
+        public Color GetContrastingColor(Color color);
         public void SetColor(Color color);
     }
 }
