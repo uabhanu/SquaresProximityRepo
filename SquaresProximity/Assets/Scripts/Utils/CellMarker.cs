9bcba6fa3c9be6a15a8c57a284c8d7302013d274
Bhanu <uabhanu@gmail.com>
2023-10-07 22:44:10 +0100

    Removed the colour logic from here and implemented that using IColourAdjuster Interface.

--- SquaresProximity/Assets/Scripts/Utils/CellMarker.cs
+++ SquaresProximity/Assets/Scripts/Utils/CellMarker.cs
@@ -1,3 +1,4 @@
+using Interfaces;
 using Managers;
 using TMPro;
 using UnityEditor;
@@ -5,7 +6,7 @@ using UnityEngine;
 
 namespace Utils
 {
-    public class CellMarker : MonoBehaviour
+    public class CellMarker : MonoBehaviour , IColourAdjuster
     {
         private GridManager _gridManager;
         private Vector2Int _currentIndex;
@@ -29,22 +30,27 @@ namespace Utils
                     indexLabelTMPObj.SetActive(true);
                     indexLabelTextTMP.text = _currentIndex.ToString();
 
-                    Color cellColor = GetCellColor(_currentIndex);
+                    Color cellColor = GetColor();
                     indexLabelTextTMP.color = GetContrastingColor(cellColor);
                 }
             
             #endif
         }
-
-        private Color GetCellColor(Vector2Int cellIndex)
-        {
-            return _gridManager.CellSpriteRenderersData.GetValue(cellIndex.x , cellIndex.y).color;
-        }
-
         private Color GetContrastingColor(Color color)
         {
             float luminance = (0.299f * color.r + 0.587f * color.g + 0.114f * color.b);
             return luminance > 0.5f ? Color.black : Color.white;
+        }
+        public Color GetColor()
+        {
+            Vector2Int cellIndex = _gridManager.WorldToCell(transform.position);
+            return _gridManager.CellSpriteRenderersData.GetValue(cellIndex.x , cellIndex.y).color;
+        }
+
+        public void SetColor(Color color)
+        {
+            Vector2Int cellIndex = _gridManager.WorldToCell(transform.position);
+            _gridManager.CellSpriteRenderersData.GetValue(cellIndex.x , cellIndex.y).color = color;
         }
     }
 }
