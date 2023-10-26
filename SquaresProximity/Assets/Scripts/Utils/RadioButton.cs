5220e9b8b89bc6c986b24a0c9e38dd69644fb7ea
BHANU-LAPTOP\uabhanu <uabhanu@gmail.com>
2023-10-26 21:37:56 +0100

    Removed the unnecessary Debug.Log messages.

--- SquaresProximity/Assets/Scripts/Utils/RadioButton.cs
+++ SquaresProximity/Assets/Scripts/Utils/RadioButton.cs
@@ -6,25 +6,21 @@ namespace Utils
     public class RadioButton : MonoBehaviour
     {
         public Toggle toggle;
-
+        
         private void Start()
         {
             if(toggle == null)
             {
-                Debug.LogError("Toggle component not assigned to RadioButton script.");
                 return;
             }
-
+        
             toggle.group = GetComponentInParent<ToggleGroup>();
             toggle.onValueChanged.AddListener(OnToggleValueChanged);
         }
-
+        
         private void OnToggleValueChanged(bool isOn)
         {
-            if(isOn)
-            {
-                Debug.Log("Toggle " + toggle.name + " is now " + (isOn ? "On" : "Off"));
-            }
+            
         }
     }
 }
