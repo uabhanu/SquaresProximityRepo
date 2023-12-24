9475e3f7099fcc7253125bba5178bd418f63a1df
BHANU-LAPTOP\uabhanu <uabhanu@gmail.com>
2023-12-24 21:22:03 +0000

    Removed the ILobbyManager interface.

--- SquaresProximity/Assets/Scripts/Interfaces/ILobbyManager.cs
+++
@@ -1,9 +0,0 @@
-namespace Interfaces
-{
-    public interface ILobbyManager
-    {
-        void CreateLobby(int maxPlayers);
-        void JoinLobby(int lobbyId);
-        void LeaveLobby();
-    }
-}
